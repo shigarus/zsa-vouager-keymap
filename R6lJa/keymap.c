20445b75ba19387c19834cb72757ccc1f2a07e2b
Aleksandr Stovbyra <shigarus@nebius.com>
2025-02-23 18:37:35 +0100

    oneshots reset leayer

--- R6lJa/keymap.c
+++ R6lJa/keymap.c
@@ -115,4 +115,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 
-
+void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (IS_QK_ONE_SHOT_MOD(keycode) && is_oneshot_layer_active() && record->event.pressed) {
+        clear_oneshot_layer_state(ONESHOT_OTHER_KEY_PRESSED);
+    }
+    return;
+}
